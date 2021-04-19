c6c4526df97bd39104fb9031d3aa88e28fb74b96
changlin93 <767425412@qq.com>
2021-04-20 07:51:11 +0800

    :sparkles: 目录构建

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -1,24 +1,45 @@
 module.exports = {
     title: 'Forest',
     description: '笔记记录、阅读',
+    serche: true,
     themeConfig: {
-        navbar: [
-            {text: '首页', link: '/'},
-            {text: '前端', link:'/front-end'},
-            {text: '数据库', link:'/database'},
-            {text: '项目', link:'/project'},
-            {text: 'Go', link:'/go'},
+        nav: [
+            { text: '首页', link: '/' },
+            {
+                text: '前端',
+                items: [
+                    { text: 'javascript', link: '/front-end/js/' },
+                    { text: 'vue', link: '/front-end/vue' },
+                    { text: 'react', link: '/front-end/react' },
+                    { text: '微信小程序', link: '/front-end/wechat' }
+                ]
+            },
+            {
+                text: '数据库',
+                items: [
+                    { text: 'mongoDB', link: '/database/mongoDB' },
+                    { text: 'redis', link: '/database/mongoDB' },
+                    { text: 'mysql', link: '/database/mongoDB' }
+                ]
+            },
+            { text: '项目', link: '/project' },
+            { text: 'Go', link: '/go' }
         ],
         sidebar: {
-            '/': [
+            '/project/': [
                 { text: '简介', link: '/' },
                 { text: '快速上手', link: '/quick-start' },
                 { text: '常见问题', link: '/faq' },
                 { text: 'API 文档', link: '/api' },
                 { text: '相关文档', link: '/link' }
+            ],
+            '/note/': [],
+            '/front-end/js/': [
+                { text: 'html中的JavaScript', link: '/front-end/js/' },
+                { text: '语言基础', link: '/front-end/js/base' }
             ]
         },
-        lastUpdated: "更新时间",
+        lastUpdated: '更新时间',
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
         repo: '',
         // 假如文档不是放在仓库的根目录下：
--- src/index.md
+++ src/index.md
@@ -1,1 +1,14 @@
-## learn Go
+---   
+home: true
+# heroImage: /images/logo.jpg
+actionText: 
+actionLink: 
+features:
+- title: 学习笔记
+  details: 好记性不如烂笔头
+- title: 个人随笔
+  details: just enjoy
+- title: 心得领悟
+  details: 学而不思则罔, 思而不学则殆
+footer: MIT Licensed | Copyright © 2021-present Forest
+--- 
