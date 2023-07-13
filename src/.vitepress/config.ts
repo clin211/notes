a509e0a2a036fd96b2cdb6f11ab4f0836a9dc7ea
forest-211 <767425412lin@gmail.com>
2023-07-13 23:39:24 +0800

    feat: add dir

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -2,28 +2,105 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    title: '',
+    title: 'Forest notes',
     description: '学习笔记、技术分享、深入思考',
+    lastUpdated: true,
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
-        siteTitle: '',
+        siteTitle: false,
+        search: {
+            provider: 'local',
+            options: {
+                locales: {
+                    zh: {
+                        translations: {
+                            button: {
+                                buttonText: '搜索文档',
+                                buttonAriaLabel: '搜索文档',
+                            },
+                            modal: {
+                                noResultsText: '无法找到相关结果',
+                                resetButtonTitle: '清除查询条件',
+                                footer: {
+                                    selectText: '选择',
+                                    navigateText: '切换',
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
         nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Examples', link: '/markdown-examples' },
-        ],
-
-        sidebar: [
             {
-                text: 'Examples',
+                text: 'front end',
                 items: [
-                    { text: 'Markdown Examples', link: '/markdown-examples' },
-                    { text: 'Runtime API Examples', link: '/api-examples' },
+                    { text: 'vue', link: '/front-end/vue/' },
+                    { text: 'react', link: '/front-end/react/' },
+                    { text: 'react-native', link: '/front-end/react-native/' },
+                    { text: 'flutter', link: '/front-end/flutter/' },
+                    { text: 'electron', link: '/front-end/electron/' },
+                    { text: 'typescript', link: '/front-end/typescript/' },
                 ],
             },
+            {
+                text: 'backend',
+                items: [
+                    {
+                        text: 'Go',
+                        items: [{ text: 'gin', link: '/backend/go/gin/' }],
+                    },
+                    {
+                        text: 'Node.js',
+                        items: [
+                            {
+                                text: 'Nest.js',
+                                link: '/backend/nodejs/nestjs/',
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                text: 'Algorithms',
+                link: '/algorithms/',
+            },
         ],
-
+        outline: 3,
+        sidebar: {
+            '/algorithms/': [
+                {
+                    text: 'Algorithms',
+                    link: '/algorithms/',
+                    items: [
+                        {
+                            text: '时间复杂度',
+                            link: '/algorithms/01-时间复杂度',
+                        },
+                        {
+                            text: '空间复杂度',
+                            link: '/algorithms/02-空间复杂度',
+                        },
+                    ],
+                },
+            ],
+            '/front-end/vue': [],
+            '/front-end/react': [],
+            '/front-end/react-native': [],
+            '/front-end/flutter': [],
+            '/front-end/electron': [],
+            '/front-end/typescript': [],
+            '/backend/go/gin/': [
+                { text: 'Restful API', link: '/backend/go/gin/01' },
+            ],
+            '/backend/nodejs/nestjs/': [],
+        },
         socialLinks: [
             { icon: 'github', link: 'https://github.com/forest-211' },
         ],
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2019-present Forest-211',
+        },
     },
 });
--- src/index.md
+++ src/index.md
@@ -4,8 +4,7 @@ layout: home
 
 hero:
   name: "Forest notes"
-  text: "A VitePress Site"
-  tagline: My great project tagline
+  tagline: 技术探索，沉淀心境，共享前后端、跨端智慧
   actions:
     - theme: brand
       text: Get Started
@@ -15,11 +14,11 @@ hero:
       link: https://github.com/forest-211
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 前端
+    details: 使用 HTML、CSS 和 JavaScript 等技术构建用户界面，创造出色的网页和应用程序，提供无缝的用户体验和交互效果。
+  - title: 服务端
+    details: 使用各种编程语言（如Go、Python、Node.js）构建服务器端应用程序，处理数据和逻辑，为前端和移动端提供功能和服务。
+  - title: 移动端/跨端
+    details: 使用技术栈如 Electron、Flutter、React Native（RN）等，构建能同时运行在多个平台（如iOS和Android）的应用程序，提供统一的用户体验和功能。
 ---
 
